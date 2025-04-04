8216e8f1bb833a44305003c93d10f9f30121ca50
cakftb <cakftb@c123.mgmt.hellbender>
2025-04-04 18:36:07 -0500

    Fixed error on stall
    The MEM function was returning when it received NOP, leaving the previous instruction in the pipeline register. This caused the previous instruction to go to the WB stage twice and therefor the instruction count counted it twice.

--- src/mu-riscv.c
+++ src/mu-riscv.c
@@ -465,17 +465,19 @@ void MEM()
 	for store: MEM[ALUOutput] <= B 
 	*/
 
-	if (EX_MEM.IR == 0) return; // No instruction to execute
+	if (EX_MEM.IR != 0) // Instruction to execute
+    {
+		uint32_t opcode = GET_OPCODE(EX_MEM.IR);
     
-    uint32_t opcode = GET_OPCODE(EX_MEM.IR);
+		if (opcode == LOAD_OPCODE) {
+			// Load instruction: Read from memory
+			MEM_WB.LMD = mem_read_32(EX_MEM.ALUOutput);
+		} else if (opcode == STORE_OPCODE) {
+			// Store instruction: Write to memory
+			mem_write_32(EX_MEM.ALUOutput, EX_MEM.B);
+		}
+	}
     
-    if (opcode == LOAD_OPCODE) {
-        // Load instruction: Read from memory
-        MEM_WB.LMD = mem_read_32(EX_MEM.ALUOutput);
-    } else if (opcode == STORE_OPCODE) {
-        // Store instruction: Write to memory
-        mem_write_32(EX_MEM.ALUOutput, EX_MEM.B);
-    }
     
     // Pass values to MEM_WB pipeline register
     MEM_WB.IR = EX_MEM.IR;

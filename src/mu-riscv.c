d53bd6ad842774077f68e54bbee778b3e7f2f755
ldhfv <ldhfv@c066.mgmt.hellbender>
2025-04-16 15:06:04 -0500

    updated hex to decimal in show_pipeline

--- src/mu-riscv.c
+++ src/mu-riscv.c
@@ -997,31 +997,32 @@ void print_b_cmd(char* cmd_name, uint8_t rs1, uint8_t rs2, uint16_t imm) {
 /* Print the current pipeline                                                                                    */
 /************************************************************/
 void show_pipeline(){
-	printf("--------------------------------------------------\n");
+    printf("--------------------------------------------------\n");
     printf("Cycle: %d\n", CYCLE_COUNT);
     printf("--------------------------------------------------\n");
 
     // Print IF/ID pipeline register
     printf("IF/ID:\n");
-    printf("  PC: 0x%08X | IR: 0x%08X\n", IF_ID.PC, IF_ID.IR);
+    printf("  PC: %d | IR: %d\n", IF_ID.PC, IF_ID.IR);
 
     // Print ID/EX pipeline register
     printf("ID/EX:\n");
-    printf("  PC: 0x%08X | IR: 0x%08X | A: 0x%08X | B: 0x%08X | imm: 0x%08X\n", 
+    printf("  PC: %d | IR: %d | A: %d | B: %d | imm: %d\n", 
             ID_EX.PC, ID_EX.IR, ID_EX.A, ID_EX.B, ID_EX.imm);
 
     // Print EX/MEM pipeline register
     printf("EX/MEM:\n");
-    printf("  PC: 0x%08X | IR: 0x%08X | ALUOutput: 0x%08X | B: 0x%08X\n", 
+    printf("  PC: %d | IR: %d | ALUOutput: %d | B: %d\n", 
             EX_MEM.PC, EX_MEM.IR, EX_MEM.ALUOutput, EX_MEM.B);
 
     // Print MEM/WB pipeline register
     printf("MEM/WB:\n");
-    printf("  PC: 0x%08X | IR: 0x%08X | ALUOutput: 0x%08X | LMD: 0x%08X\n", 
+    printf("  PC: %d | IR: %d | ALUOutput: %d | LMD: %d\n", 
             MEM_WB.PC, MEM_WB.IR, MEM_WB.ALUOutput, MEM_WB.LMD);
 
     printf("--------------------------------------------------\n");
 }
+
 
 /***************************************************************/
 /* main                                                                                                                                   */
